f3da4709fecd21a92c70a04f86ad19a71583d723
IRONenot <avdakoff.sanya@yandex.ru>
2023-12-21 11:48:01 +0300

    btn not adapted

--- btn.js
+++ btn.js
@@ -1,39 +1,116 @@
 const btn_1 = document.getElementById("btn_1");
 
-let start_1 = 0;
-
-const colors_1 = ["maroon", "blue"];
+let curColor_1 = "red";
 
 btn_1.addEventListener("click", function onClick() {
-  console.log('btn_1');
-  btn_1.style.backgroundColor = 'black'
-  //const curColor = btn_1.style.backgroundColor
-  //console.log('curColor=', curColor);
-   
+  if (curColor_1 === "red") {
+    curColor_1 = "maroon";
+  } else {
+    curColor_1 = "red";
+  }
+  btn_1.style.backgroundColor = curColor_1;
 });
 
 const btn_2 = document.getElementById("btn_2");
 
-let curColor = 'pink';
-
+let curColor_2 = "orange";
 
 btn_2.addEventListener("click", function onClick() {
-  if (curColor === 'pink'){
-    curColor = 'orange'
+  if (curColor_2 === "orange") {
+    curColor_2 = "orangered";
   } else {
-    curColor = 'pink'
+    curColor_2 = "orange";
   }
-  btn_2.style.backgroundColor = curColor
-  
+  btn_2.style.backgroundColor = curColor_2;
 });
 
 const btn_3 = document.getElementById("btn_3");
 
-let start_3 = 0;
-
-const colors_3 = ["yellowgreen", "yellow"];
+let curColor_3 = "yellow";
 
 btn_3.addEventListener("click", function onClick() {
-  btn_3.style.backgroundColor = colors_3[start_3];
-  start_3 = start_3 >= colors_3.length - 1 ? 0 : start_3 + 1;
+  if (curColor_3 === "yellow") {
+    curColor_3 = "yellowgreen";
+  } else {
+    curColor_3 = "yellow";
+  }
+  btn_3.style.backgroundColor = curColor_3;
+});
+
+const btn_4 = document.getElementById("btn_4");
+
+let curColor_4 = "green";
+
+btn_4.addEventListener("click", function onClick() {
+  if (curColor_4 === "green") {
+    curColor_4 = "greenyellow";
+  } else {
+    curColor_4 = "green";
+  }
+  btn_4.style.backgroundColor = curColor_4;
+});
+
+const btn_5 = document.getElementById("btn_5");
+
+let curColor_5 = "skyblue";
+
+btn_5.addEventListener("click", function onClick() {
+  if (curColor_5 === "skyblue") {
+    curColor_5 = "aqua";
+  } else {
+    curColor_5 = "skyblue";
+  }
+  btn_5.style.backgroundColor = curColor_5;
+});
+
+const btn_6 = document.getElementById("btn_6");
+
+let curColor_6 = "blue";
+
+btn_6.addEventListener("click", function onClick() {
+  if (curColor_6 === "blue") {
+    curColor_6 = "darkblue";
+  } else {
+    curColor_6 = "blue";
+  }
+  btn_6.style.backgroundColor = curColor_6;
+});
+
+const btn_7 = document.getElementById("btn_7");
+
+let curColor_7 = "violet";
+
+btn_7.addEventListener("click", function onClick() {
+  if (curColor_7 === "violet") {
+    curColor_7 = "blueviolet";
+  } else {
+    curColor_7 = "violet";
+  }
+  btn_7.style.backgroundColor = curColor_7;
+});
+
+const btn_8 = document.getElementById("btn_8");
+
+let curColor_8 = "black";
+
+btn_8.addEventListener("click", function onClick() {
+  if (curColor_8 === "black") {
+    curColor_8 = "gray";
+  } else {
+    curColor_8 = "black";
+  }
+  btn_8.style.backgroundColor = curColor_8;
+});
+
+const btn_9 = document.getElementById("btn_9");
+
+let curColor_9 = "brown";
+
+btn_9.addEventListener("click", function onClick() {
+  if (curColor_9 === "brown") {
+    curColor_9 = "pink";
+  } else {
+    curColor_9 = "brown";
+  }
+  btn_9.style.backgroundColor = curColor_9;
 });
