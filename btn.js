5f7d56f09770d3acf0c9bdc04842615dce51db92
IRONenot <avdakoff.sanya@yandex.ru>
2023-12-29 20:04:47 +0300

    modification

--- btn.js
+++ btn.js
@@ -1,132 +1,20 @@
-const btn_1 = document.getElementById("btn_1")
+const btn_1 = document.querySelector('#btn_1')
+const btn_2 = document.querySelector('#btn_2')
+const btn_3 = document.querySelector('#btn_3')
+const btn_4 = document.querySelector('#btn_4')
+const btn_5 = document.querySelector('#btn_5')
+const btn_6 = document.querySelector('#btn_6')
+const btn_7 = document.querySelector('#btn_7')
+const btn_8 = document.querySelector('#btn_8')
+const btn_9 = document.querySelector('#btn_9')
 
-let curColor_1 = "red";
+btn_1.addEventListener( 'click', () => btn_1.style.backgroundColor = 'maroon')
+btn_2.addEventListener( 'click', () => btn_2.style.backgroundColor = 'orangered')
+btn_3.addEventListener( 'click', () => btn_3.style.backgroundColor = 'yellowgreen')
+btn_4.addEventListener( 'click', () => btn_4.style.backgroundColor = 'greenyellow')
+btn_5.addEventListener( 'click', () => btn_5.style.backgroundColor = 'aqua')
+btn_6.addEventListener( 'click', () => btn_6.style.backgroundColor = 'darkblue')
+btn_7.addEventListener( 'click', () => btn_7.style.backgroundColor = 'blueviolet')
+btn_8.addEventListener( 'click', () => btn_8.style.backgroundColor = 'grey')
+btn_9.addEventListener( 'click', () => btn_9.style.backgroundColor = 'pink')
 
-btn_1.addEventListener("click", function onClick() {
-  if (curColor_1 === "red") {
-    curColor_1 = "maroon";
-  } else {
-    curColor_1 = "red";
-  }
-  btn_1.style.backgroundColor = curColor_1;
-});
-
-//============================
-
-const btn_2 = document.getElementById("btn_2");
-
-let curColor_2 = "orange";
-
-btn_2.addEventListener("click", function onClick() {
-  if (curColor_2 === "orange") {
-    curColor_2 = "orangered";
-  } else {
-    curColor_2 = "orange";
-  }
-  btn_2.style.backgroundColor = curColor_2;
-});
-
-//============================
-
-const btn_3 = document.getElementById("btn_3");
-
-let curColor_3 = "yellow";
-
-btn_3.addEventListener("click", function onClick() {
-  if (curColor_3 === "yellow") {
-    curColor_3 = "yellowgreen";
-  } else {
-    curColor_3 = "yellow";
-  }
-  btn_3.style.backgroundColor = curColor_3;
-});
-
-//============================
-
-const btn_4 = document.getElementById("btn_4");
-
-let curColor_4 = "green";
-
-btn_4.addEventListener("click", function onClick() {
-  if (curColor_4 === "green") {
-    curColor_4 = "greenyellow";
-  } else {
-    curColor_4 = "green";
-  }
-  btn_4.style.backgroundColor = curColor_4;
-});
-
-//============================
-
-const btn_5 = document.getElementById("btn_5");
-
-let curColor_5 = "skyblue";
-
-btn_5.addEventListener("click", function onClick() {
-  if (curColor_5 === "skyblue") {
-    curColor_5 = "aqua";
-  } else {
-    curColor_5 = "skyblue";
-  }
-  btn_5.style.backgroundColor = curColor_5;
-});
-
-//============================
-
-const btn_6 = document.getElementById("btn_6");
-
-let curColor_6 = "blue";
-
-btn_6.addEventListener("click", function onClick() {
-  if (curColor_6 === "blue") {
-    curColor_6 = "darkblue";
-  } else {
-    curColor_6 = "blue";
-  }
-  btn_6.style.backgroundColor = curColor_6;
-});
-
-//============================
-
-const btn_7 = document.getElementById("btn_7");
-
-let curColor_7 = "violet";
-
-btn_7.addEventListener("click", function onClick() {
-  if (curColor_7 === "violet") {
-    curColor_7 = "blueviolet";
-  } else {
-    curColor_7 = "violet";
-  }
-  btn_7.style.backgroundColor = curColor_7;
-});
-
-//============================
-
-const btn_8 = document.getElementById("btn_8");
-
-let curColor_8 = "black";
-
-btn_8.addEventListener("click", function onClick() {
-  if (curColor_8 === "black") {
-    curColor_8 = "gray";
-  } else {
-    curColor_8 = "black";
-  }
-  btn_8.style.backgroundColor = curColor_8;
-});
-
-//============================
-
-const btn_9 = document.getElementById("btn_9");
-
-let curColor_9 = "brown";
-
-btn_9.addEventListener("click", function onClick() {
-  if (curColor_9 === "brown") {
-    curColor_9 = "pink";
-  } else {
-    curColor_9 = "brown";
-  }
-  btn_9.style.backgroundColor = curColor_9;
-});
